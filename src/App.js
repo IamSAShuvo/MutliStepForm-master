a552217dc1532e500e5df14c52ae5aabb56635d1
Md. Salman Aziz Shuvo <salmanazizshuvo@gmail.com>
2024-02-13 21:07:29 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import {MutliStepForm} from './component/MutliStepForm';
+
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <MutliStepForm/>
+    </div>
+  );
+}
+
+export default App;
